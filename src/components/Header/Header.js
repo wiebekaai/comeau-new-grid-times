95d06e716019372ded7f56a2473a956547f5ce58
Wiebe Kaai <wiebekaai@gmail.com>
2021-11-29 12:54:58 +0100

    Responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -21,6 +21,13 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
+          <SuperHeaderLogo />
+          <SubscribeContainer>
+            <Button>
+              Subscribe
+            </Button>
+            <Link href="/">Already a subscriber?</Link>
+          </SubscribeContainer>
           <ActionGroup>
             <button>
               <User size={24} />
@@ -39,11 +46,49 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    background: none;
+    color: var(--color-gray-900);
+    margin-bottom: 48px;
+  }
+`;
+
+const SuperHeaderLogo = styled(Logo)`
+  display: none;
+  color: var(--color-offblack);
+  --font-size: ${64/16}rem;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+`;
+
+const SubscribeContainer = styled.div`
+  display: none;
+  flex-direction: column;
+  align-items:center;
+  gap: 8px;
+  justify-content: center;
+  align-self: flex-end;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const Link = styled.a`
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.normal};
+  font-size: ${14 / 16}rem;
+  line-height: 1.5;
+  font-style: italic;
+  text-decoration: underline;
 `;
 
 const ActionGroup = styled.div`
@@ -57,6 +102,12 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
+
+  &:last-of-type {
+    @media ${QUERIES.laptopAndUp} {
+      display: none;
+    }
+  }
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
@@ -65,6 +116,10 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 export default Header;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import format from 'date-fns/format';
 
-const Logo = (props) => {
+const Logo = ( props) => {
   return (
-    <Wrapper>
-      <Link href="/" {...props}>
+    <Wrapper {...props}>
+      <Link href="/">
         New Grid Times
       </Link>
       <TodaysDate>
@@ -16,12 +16,13 @@ const Logo = (props) => {
 };
 
 const Wrapper = styled.div`
+  --font-size: 3rem;
   text-align: center;
 `;
 
 const Link = styled.a`
   font-family: var(--font-family-logo);
-  font-size: 3rem;
+  font-size: var(--font-size);
 `;
 
 const TodaysDate = styled.p`
